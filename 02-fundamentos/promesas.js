dde9808c97d8e9e36fe7b95756d83abe5fa95725
Agustin Moya <agustin.moya@despegar.com>
2018-09-20 09:22:20 -0300

    Callbacks y promises

---
+++ 02-fundamentos/promesas.js
@@ -0,0 +1,63 @@
+let empleados = [{
+        id: 1,
+        nombre: 'Agustin'
+    }, {
+        id: 2,
+        nombre: 'Melissa'
+    },
+    {
+        id: 3,
+        nombre: 'Juan'
+    }
+]
+
+let salarios = [{
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 2000
+    }
+];
+
+let getEmpleado = (id) => {
+
+
+    return new Promise((resolve, reject) => {
+        let empleadoDB = empleados.find(empleado => (empleado.id === id))
+
+        if (!empleadoDB) {
+            reject(`No existe un empleado con el ID ${ id }`);
+        } else {
+            resolve(empleadoDB);
+        }
+    })
+
+}
+
+getEmpleado(1).then(empleado => {
+    getSalario(empleado).then(result => {
+        console.log(result);
+    }, (err) => console.log(err))
+}, (err) => console.log(err))
+
+
+let getSalario = (empleado) => {
+
+    return new Promise((resolve, reject) => {
+
+        let salarioDB = salarios.find(salario => salario.id === empleado.id)
+
+        if (!salarioDB) {
+            reject(`No se encontro un salario para el usuario ${empleado.nombre}`)
+        } else {
+            resolve({
+                nombre: empleado.nombre,
+                salario: salarioDB.salario,
+                id: empleado.id
+            })
+        }
+
+    })
+}
