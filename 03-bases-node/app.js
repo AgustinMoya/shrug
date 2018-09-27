2176c16e50b194b87e3e7948ecb586b64cfbea69
Agustin Moya <agustin.moya@despegar.com>
2018-09-27 18:35:14 -0300

    Se agregaron colores a la respuesta en consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,29 +1,8 @@
-const argv = require('yargs')
-
-.command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        },
-    })
-    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        },
-    })
-    .help()
-    .argv;
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
+
 
 let comando = argv._[0];
 
@@ -34,8 +13,8 @@ switch (comando) {
 
     case 'crear':
         crearArchivo(argv.base, argv.limite)
-            .then(archivo => console.log(`Archivo creado: ${archivo}, con limite ${argv.limite}`))
-            .catch(e => console.log(e))
+            .then(archivo => console.log('Archivo creado:' + colors.yellow(`${archivo}, con limite ${argv.limite}`)))
+            .catch(e => console.log(e));
         break;
     default:
         console.log('Comando no reconocido');
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
+const colors = require('colors');
 
 let listarTabla = (base, limite = 10) => {
+
+    console.log('===================='.rainbow);
+    console.log(`tabla de ${base}`.green);
+    console.log('===================='.rainbow);
     for (let i = 1; i <= limite; i++) {
         console.log(`${base} * ${i} = ${base*i}`);
     }
