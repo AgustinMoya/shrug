2ca1127a0071822f59918221751b495b423b2fc8
Agustin Moya <agustin.moya@despegar.com>
2018-10-05 23:17:56 -0300

    Add start script

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -5,27 +5,13 @@ const hbs = require('hbs')
 
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/public'))
 
 //Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales')
 app.set('view engine', 'hbs');
-
-
-//helpers
-hbs.registerHelper('getAnio', () => {
-    return new Date().getFullYear();
-});
-
-hbs.registerHelper('capitalizar', (texto) => {
-    let palabras = texto.split(' ');
-    palabras.forEach((palabra, idx) => {
-        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase()
-    })
-
-    return palabras.join(' ')
-});
-
 
 app.get('/', (req, res) => {
 
@@ -39,6 +25,6 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
