cf8a7ab4fd88950f783f6b6654ceab14b77c2b7d
Agustin Moya <agustin.moya@despegar.com>
2018-10-11 09:31:27 -0300

    Set mongo url in heroku

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -8,6 +8,6 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
-} else urlDB = 'mongodb://elagusAMG:asd123@ds127843.mlab.com:27843/cafe'
+} else urlDB = process.env.MONGO_URI
 
 process.env.URLDB = urlDB;
