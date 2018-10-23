0a85bd1959b679bfe9f46a45b6e28e527af413ce
Agustin Moya <agustin.moya@despegar.com>
2018-10-23 14:32:13 -0300

    TODO: implement categoria.js

---
+++ 07-restserver/server/controllers/categoria.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+let { verificaToken } = require('../middlewares/autenticacion');
+
+let app = express();
+
+let Categoria = require('../models/categoria')
+
+// ============================
+// Mostrar todas las categorias
+// ============================
+app.get('/categoria', (req, res) => {});
+
+
+// ============================
+// Mostrar una categoria por ID
+// ============================
+app.get('/categoria/:id', (req, res) => {
+
+    // Categoria.findById(...);
+});
+
+// ============================
+// Crear nueva categoria
+// ============================
+app.post('/categoria', verificaToken, (req, res) => {
+
+    // Regresar nueva categoria
+    // req.usuario._id ID DE LA PERSONA Q EJECUTA LA FUNCION CON TOKEN VALIDO
+});
+
+// ============================
+// Mostrar todas las categorias
+// ============================
+app.put('/categoria/:id', (req, res) => {
+
+    // Actualizar descripcion categoria
+})
+
+// ============================
+// Mostrar todas las categorias
+// ============================
+app.get('/categoria/:id', (req, res) => {
+
+    // solo un administrador puede borrar categorias
+    // Categoria.findByIdAndRemove(...)
+
+})
+
+module.exports = app;
